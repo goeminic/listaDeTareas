1a841cfa5a1617901d2622d4c610d2653f9119bc
goeminic <gnico2127@gmail.com>
2024-08-08 16:16:36 -0300

    final

--- script.js
+++ script.js
@@ -17,13 +17,36 @@ const setDate = () => {
 const addNewTask = event =>{
     event.preventDefault();
     const { value } = event.target.taskText;
-    if (!value) return;
+    if (!value){ window.alert('El campo se encuentra vacio'); return;} else{
     const task = document.createElement('div');
     task.classList.add('task','roundBorder');
     task.addEventListener('click',changeTaskState);
     task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+    }
+};
+
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
 
 };
+
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el));
+};
+
+
 
 setDate();
 
